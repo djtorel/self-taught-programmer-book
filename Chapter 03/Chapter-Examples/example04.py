17e3a076cb2d74d32474d2fa16a6638d790780a8
Dominic Torruellas <dtorruellas@gmail.com>
2017-09-01 16:00:46 -0700

    Chapter 03, Examples

---
+++ Chapter 03/Chapter-Examples/example04.py
@@ -0,0 +1,21 @@
+home = "Thailand"
+if home == "Japan":
+    print("Hello, Japan!")
+elif home == "Thailand":
+    print("Hello, Thailand!")
+elif home == "China":
+    print("Hello, China!")
+else:
+    print("Hello, World!")
+
+home = "Mars"
+if home == "America":
+    print("Hello, America!")
+elif home == "Canada":
+    print("Hello, Canada!")
+elif home == "Thailand":
+    print("Hello, Thailand!")
+elif home == "Mexico":
+    print("Hello, Mexico!")
+else:
+    print("Hello, World!")
