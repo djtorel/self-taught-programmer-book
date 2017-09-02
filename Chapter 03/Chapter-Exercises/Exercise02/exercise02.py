62563e5d20b3bc72d7f71e5cdc16673cc04e4089
Dominic Torruellas <dtorruellas@gmail.com>
2017-09-01 17:18:35 -0700

    Chapter 03, Exercise 02

---
+++ Chapter 03/Chapter-Exercises/Exercise02/exercise02.py
@@ -0,0 +1,11 @@
+"""
+Write a program that prints a message if a variable is less than 10, and
+different message if the variable is greater than or equal to 10.
+"""
+
+num1 = 9
+
+if num1 < 10:
+    print("num1 is less than 10")
+else:
+    print("num1 is greater than or equal to 10")
