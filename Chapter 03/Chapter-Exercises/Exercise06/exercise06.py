d9f0a87a71758de882616982f8d42d74562f0722
Dominic Torruellas <dtorruellas@gmail.com>
2017-09-01 17:48:13 -0700

    Chapter 03, Exercise 06

---
+++ Chapter 03/Chapter-Exercises/Exercise06/exercise06.py
@@ -0,0 +1,13 @@
+"""
+Write a program with a variable age assigned to an integer that prints different
+strings depending on what integer age is.
+"""
+
+age = 23
+
+if age <= 40:
+    print("You're still young!")
+elif age <= 60:
+    print("You're middle aged!")
+else:
+    print("Time for retirement!")
