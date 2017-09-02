367c2f8b38ff766d1bc3f1d460cb2232e8f52122
Dominic Torruellas <dtorruellas@gmail.com>
2017-09-02 12:04:34 -0700

    Chapter 04, Exercise 02

---
+++ Chapter 04/Chapter-Exercises/Exercise02/exercise02.py
@@ -0,0 +1,11 @@
+"""
+Create a function that accepts a string as a parameter and prints it.
+"""
+
+
+def message(message_text):
+    print(message_text)
+
+
+message("Hello")
+message("This works!")
