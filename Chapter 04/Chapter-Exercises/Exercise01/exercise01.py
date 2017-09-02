c268c3e2a9519d7c9fba3873f7d60aa5772a37d2
Dominic Torruellas <dtorruellas@gmail.com>
2017-09-02 12:01:22 -0700

    Chapter 04, Exercise 01

---
+++ Chapter 04/Chapter-Exercises/Exercise01/exercise01.py
@@ -0,0 +1,14 @@
+"""
+Write a function that takes a number as an input and returns that number squared.
+"""
+
+
+def squared(x):
+    return x * x
+
+
+print(squared(2))
+print(squared(4))
+print(squared(5))
+print(squared(8))
+print(squared(9))
